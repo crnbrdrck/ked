2f9c452bb40a14962bc98e04c6f3511437c621b2
Ciaran Broderick <broderickciaran@gmail.com>
2018-06-11 22:45:58 +0100

    Moved lexing methods from the interpreter class to a new Lexer class

--- src/ked/interpreter.cr
+++ src/ked/interpreter.cr
@@ -1,156 +1,57 @@
 # TODO: Documentation
 module Ked
+  # Grammar rules
+  # expr:   factor ((MUL | DIV) factor)*
+  # factor: INTEGER
   class Interpreter
-    @text : String
-    @current_char : Char
-    @pos : Int64 = 0_i64
-    @current_token : Token = Token.new TokenType::SOF, '\u{0}'
+    @lexer : Lexer
+    @current_token : Token
 
-    def initialize(@text : String)
-      if @text.size != 0
-        @current_char = @text[@pos]
-      else
-        @current_char = '\u{0}'
-      end
+    def initialize(text : String)
+      @lexer = Lexer.new text
+      @current_token = @lexer.get_next_token
     end
 
     def error
-      raise "Error parsing input"
-    end
-
-    # Advance the 'pos' pointer and set the 'current_char' variable
-    def advance
-      @pos += 1
-      if @pos > @text.size - 1
-        @current_char = '\u{0}'
-      else
-        @current_char = @text[@pos]
-      end
-    end
-
-    def skip_whitespace
-      while @current_char != '\u{0}' && @current_char == ' '
-        self.advance
-      end
-    end
-
-    # Return a multidigit integer consumed from the text
-    def get_integer : Int
-      result = [] of Char
-      while @current_char != '\u{0}' && @current_char.to_i?
-        result << @current_char
-        self.advance
-      end
-      result.join("").to_i
-    end
-
-    # Lexical analyser (also known as scanner or tokeniser)
-    #
-    # This method is responsible for breaking a sentence apart into tokens. One token at a time.
-    def get_next_token : Token
-      # Loop through and generate tokens from the text
-      while @current_char != '\u{0}'
-        # Check for whitespace first
-        if @current_char == ' '
-          self.skip_whitespace
-          next
-        elsif @current_char.to_i?
-          return Token.new TokenType::INTEGER, self.get_integer
-        elsif @current_char == '+'
-          self.advance
-          return Token.new TokenType::PLUS, '+'
-        elsif @current_char == '-'
-          self.advance
-          return Token.new TokenType::MINUS, '-'
-        elsif @current_char == '*'
-          self.advance
-          return Token.new TokenType::MULTIPLY, '*'
-        elsif @current_char == '/'
-          self.advance
-          return Token.new TokenType::DIVIDE, '/'
-        end
-        self.error
-      end
-      Token.new TokenType::EOF, '\u{0}'
+      raise "Error when parsing input"
     end
 
     # Compare the current token type with the passed token type and if they match then "eat" the current token and assign the next token to current token, otherwise raise an exception
     def eat(token_type : TokenType)
       if @current_token.token_type == token_type
-        @current_token = self.get_next_token
+        @current_token = @lexer.get_next_token
       else
         self.error
       end
     end
 
-    # Get the next integer
-    def term : Int
+    # Grammar rules implementations
+    # factor: INTEGER
+    def factor : Int
       token = @current_token
       self.eat TokenType::INTEGER
-      return token.value.to_i
+      token.value.to_i
     end
 
-    # Parser / Interpreter
-    def expr
-      # Set current token to first token from text
-      @current_token = self.get_next_token
-
-      # Get our first integer
-      result = self.term
-      # Keep looping through the syntax diagram
-      loop_tokens = [
-        TokenType::PLUS,
-        TokenType::MINUS,
+    # expr: factor ((MUL | DIV) factor)*
+    def expr : Int
+      token_types = [
         TokenType::MULTIPLY,
         TokenType::DIVIDE,
       ]
-      while loop_tokens.includes? @current_token.token_type
-        token = @current_token
-        case token.token_type
-        when TokenType::PLUS
-          self.eat TokenType::PLUS
-          result += self.term
-        when TokenType::MINUS
-          self.eat TokenType::MINUS
-          result -= self.term
-        when TokenType::MULTIPLY
+      # Get the first factor for our expr (which is not optional according to our grammar rules)
+      result = self.factor
+      while token_types.includes? @current_token.token_type
+        # Depending on which symbol our current token is currently on, do some maths
+        if @current_token.token_type == TokenType::MULTIPLY
           self.eat TokenType::MULTIPLY
-          result *= self.term
-        when TokenType::DIVIDE
+          result *= self.factor
+        elsif @current_token.token_type == TokenType::DIVIDE
           self.eat TokenType::DIVIDE
-          result /= self.term
+          result /= self.factor
         end
       end
-      # Return the result
       result
-    end
-
-    def eat_operator(op : Token)
-      case op.token_type
-      when TokenType::PLUS
-        self.eat TokenType::PLUS
-      when TokenType::MINUS
-        self.eat TokenType::MINUS
-      when TokenType::MULTIPLY
-        self.eat TokenType::MULTIPLY
-      when TokenType::DIVIDE
-        self.eat TokenType::DIVIDE
-      end
-    end
-
-    def handle_expression(left : Int, op : Token, right : Int) : Int
-      result = 0
-      case op.token_type
-      when TokenType::PLUS
-        result += left + right
-      when TokenType::MINUS
-        result += left - right
-      when TokenType::MULTIPLY
-        result += left * right
-      when TokenType::DIVIDE
-        result += left / right
-      end
-      return result
     end
   end
 end
---
+++ src/ked/lexer.cr
@@ -0,0 +1,74 @@
+module Ked
+  class Lexer
+    @current_char : Char
+    @pos : Int64 = 0_i64
+
+    def initialize(@text : String)
+      if @text.size != 0
+        @current_char = @text[@pos]
+      else
+        @current_char = '\u{0}'
+      end
+    end
+
+    def error
+      raise "Error when parsing input: position #{@pos}"
+    end
+
+    # Advance the 'pos' pointer and set the 'current_char' variable
+    def advance
+      @pos += 1
+      if @pos > @text.size - 1
+        @current_char = '\u{0}'
+      else
+        @current_char = @text[@pos]
+      end
+    end
+
+    def skip_whitespace
+      while @current_char != '\u{0}' && @current_char == ' '
+        self.advance
+      end
+    end
+
+    # Return a multidigit integer consumed from the text
+    def get_integer : Int
+      result = [] of Char
+      while @current_char != '\u{0}' && @current_char.to_i?
+        result << @current_char
+        self.advance
+      end
+      result.join("").to_i
+    end
+
+    # Lexical analyser (also known as scanner or tokeniser)
+    #
+    # This method is responsible for breaking a sentence apart into tokens. One token at a time.
+    def get_next_token : Token
+      # Loop through and generate tokens from the text
+      while @current_char != '\u{0}'
+        # Check for whitespace first
+        if @current_char == ' '
+          self.skip_whitespace
+          next
+        elsif @current_char.to_i?
+          return Token.new TokenType::INTEGER, self.get_integer
+        elsif @current_char == '+'
+          self.advance
+          return Token.new TokenType::PLUS, '+'
+        elsif @current_char == '-'
+          self.advance
+          return Token.new TokenType::MINUS, '-'
+        elsif @current_char == '*'
+          self.advance
+          return Token.new TokenType::MULTIPLY, '*'
+        elsif @current_char == '/'
+          self.advance
+          return Token.new TokenType::DIVIDE, '/'
+        end
+        self.error
+      end
+      Token.new TokenType::EOF, '\u{0}'
+    end
+  end
+end
