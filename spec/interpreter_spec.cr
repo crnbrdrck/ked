39b7495d6ba16ce032e2454241668a6bc5e93a84
Ciaran Broderick <broderickciaran@gmail.com>
2018-06-11 23:49:42 +0100

    added spec for brackets based arithmetic

--- spec/interpreter_spec.cr
+++ spec/interpreter_spec.cr
@@ -50,6 +50,11 @@ describe Ked::Interpreter do
       interpreter.expr.should eq 17
     end
 
+    it "should be able to handle complex arithmetic expressions that include brackets" do
+      interpreter = Ked::Interpreter.new "3 + (4 * (5 - 2))"
+      interpreter.expr.should eq 15
+    end
+
     it "should throw an error if the expression does match either of these patterns" do
       interpreter = Ked::Interpreter.new "7 x 3"
       expect_raises(Exception) do
