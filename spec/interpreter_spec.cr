e8b1512f6f996a975e3c77053879100c744bfb89
Ciaran Broderick <broderickciaran@gmail.com>
2018-06-12 21:33:24 +0100

    Refactored the interpreter spec in preparation for the ked syntax to be actually usable

--- spec/interpreter_spec.cr
+++ spec/interpreter_spec.cr
@@ -3,37 +3,37 @@ require "./spec_helper"
 describe Ked::Interpreter do
   describe "interpret" do
     it "should be successfully be able to handle an expression of INTEGER PLUS INTEGER and return the correct value" do
-      interpreter = Ked::Interpreter.new "3 + 3"
+      interpreter = Ked::Interpreter.new "3 plus 3"
       interpreter.interpret.should eq 6
     end
 
-    it "should be successfully be able to handle an expression of INTEGER MINUS INTEGER and return the correct value" do
-      interpreter = Ked::Interpreter.new "7 - 3"
+    it "should be successfully be able to handle an expression of INTEGER AWAY_FROM INTEGER and return the correct value" do
+      interpreter = Ked::Interpreter.new "3 awayFrom 7"
       interpreter.interpret.should eq 4
     end
 
-    it "should be successfully be able to handle an expression of INTEGER MULTIPLY INTEGER and return the correct value" do
-      interpreter = Ked::Interpreter.new "7 * 3"
+    it "should be successfully be able to handle an expression of INTEGER TIMES INTEGER and return the correct value" do
+      interpreter = Ked::Interpreter.new "7 times 3"
       interpreter.interpret.should eq 21
     end
 
-    it "should be successfully be able to handle an expression of INTEGER DIVIDE INTEGER and return the correct value" do
-      interpreter = Ked::Interpreter.new "6 / 3"
+    it "should be successfully be able to handle an expression of INTEGER INTO INTEGER and return the correct value" do
+      interpreter = Ked::Interpreter.new "3 into 6"
       interpreter.interpret.should eq 2
     end
 
     it "should handle multiple additions and subtractions" do
-      interpreter = Ked::Interpreter.new "3 + 3 + 3 - 7"
+      interpreter = Ked::Interpreter.new "7 awayFrom 3 plus 3 plus 3"
       interpreter.interpret.should eq 2
     end
 
     it "should be able to handle complex arithmetic expressions using BEMDAS rule" do
-      interpreter = Ked::Interpreter.new "14 + 2 * 3 - 6 / 2"
+      interpreter = Ked::Interpreter.new "14 plus 2 into 6 awayFrom 2 times 3"
       interpreter.interpret.should eq 17
     end
 
     it "should be able to handle complex arithmetic expressions that include brackets" do
-      interpreter = Ked::Interpreter.new "3 + (4 * (5 - 2))"
+      interpreter = Ked::Interpreter.new "3 plus (4 times (2 awayFrom 5))"
       interpreter.interpret.should eq 15
     end
 
