7a5d0700ee9c6801aa1a671a80d9270b63b3923b
Ciaran Broderick <broderickciaran@gmail.com>
2018-06-12 22:27:15 +0100

    Created a NoOp node for handling empty statements.
    Not sure if I ***really*** need it so its currently only here since it
    is outlined in the tutorial.
    
    If it turns out I don't need to handle empty statements then I have no
    issues with removing this node altogether

---
+++ src/ked/ast/no_op.cr
@@ -0,0 +1,6 @@
+module Ked
+  # Node representing an empty statement
+  # NOTE: This might be removed at a later point if no use is found for it, it is here as it was pointed out in the tutorial and there might be a use for it yet
+  class NoOp < AST
+  end
+end
