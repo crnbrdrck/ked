061ab1ad2f7d37d8a14a3b5a48e7925ea71838bc
Ciaran Broderick <broderickciaran@gmail.com>
2018-06-12 22:00:54 +0100

    Added a spec to test that the lexer can actually understand basic Ked statements

--- spec/lexer_spec.cr
+++ spec/lexer_spec.cr
@@ -51,5 +51,18 @@ describe Ked::Lexer do
       expected = Ked::Token.new Ked::TokenType::CLOSE_PAREN, ')'
       Ked::Lexer.new(")").get_next_token.should eq expected
     end
+
+    it "should be able to understand a proper statement and turn it into correct lexemes" do
+      text = "remember €x = 2 like"
+      lexer = Ked::Lexer.new(text)
+      # Tokens in this order: REMEMBER VAR_PREFIX ID ASSIGN INTEGER LIKE
+      lexer.get_next_token.should eq Ked::Token.new(Ked::TokenType::REMEMBER, "remember")
+      lexer.get_next_token.should eq Ked::Token.new(Ked::TokenType::VAR_PREFIX, '€')
+      lexer.get_next_token.should eq Ked::Token.new(Ked::TokenType::ID, "x")
+      lexer.get_next_token.should eq Ked::Token.new(Ked::TokenType::ASSIGN, '=')
+      lexer.get_next_token.should eq Ked::Token.new(Ked::TokenType::INTEGER, 2)
+      lexer.get_next_token.should eq Ked::Token.new(Ked::TokenType::LIKE, "like")
+      lexer.get_next_token.should eq Ked::Token.new(Ked::TokenType::EOF, Ked::TERMINATOR)
+    end
   end
 end
