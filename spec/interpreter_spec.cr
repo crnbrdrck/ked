a7d558a96e878a74397cf116dd7bee589dbf9377
Ciaran Broderick <broderickciaran@gmail.com>
2018-06-13 01:29:09 +0100

    Fixed the interpreter specs to run with the new version of the interpreter

--- spec/interpreter_spec.cr
+++ spec/interpreter_spec.cr
@@ -3,42 +3,49 @@ require "./spec_helper"
 describe Ked::Interpreter do
   describe "interpret" do
     it "should be successfully be able to handle an expression of INTEGER PLUS INTEGER and return the correct value" do
-      interpreter = Ked::Interpreter.new "3 plus 3"
-      interpreter.interpret.should eq 6
+      interpreter = Ked::Interpreter.new "remember €x = 3 plus 3 like"
+      interpreter.interpret
+      interpreter.global_scope["x"].should eq 6
     end
 
     it "should be successfully be able to handle an expression of INTEGER AWAY_FROM INTEGER and return the correct value" do
-      interpreter = Ked::Interpreter.new "3 awayFrom 7"
-      interpreter.interpret.should eq 4
+      interpreter = Ked::Interpreter.new "remember €x = 3 awayFrom 7 like"
+      interpreter.interpret
+      interpreter.global_scope["x"].should eq 4
     end
 
     it "should be successfully be able to handle an expression of INTEGER TIMES INTEGER and return the correct value" do
-      interpreter = Ked::Interpreter.new "7 times 3"
-      interpreter.interpret.should eq 21
+      interpreter = Ked::Interpreter.new "remember €x = 7 times 3 like"
+      interpreter.interpret
+      interpreter.global_scope["x"].should eq 21
     end
 
     it "should be successfully be able to handle an expression of INTEGER INTO INTEGER and return the correct value" do
-      interpreter = Ked::Interpreter.new "3 into 6"
-      interpreter.interpret.should eq 2
+      interpreter = Ked::Interpreter.new "remember €x = 3 into 6 like"
+      interpreter.interpret
+      interpreter.global_scope["x"].should eq 2
     end
 
     it "should handle multiple additions and subtractions" do
-      interpreter = Ked::Interpreter.new "7 awayFrom 3 plus 3 plus 3"
-      interpreter.interpret.should eq 2
+      interpreter = Ked::Interpreter.new "remember €x = 7 awayFrom 3 plus 3 plus 3 like"
+      interpreter.interpret
+      interpreter.global_scope["x"].should eq 2
     end
 
     it "should be able to handle complex arithmetic expressions using BEMDAS rule" do
-      interpreter = Ked::Interpreter.new "14 plus 2 into 6 awayFrom 2 times 3"
-      interpreter.interpret.should eq 17
+      interpreter = Ked::Interpreter.new "remember €x = 14 plus (2 into 6 awayFrom 2 times 3) like"
+      interpreter.interpret
+      interpreter.global_scope["x"].should eq 17
     end
 
     it "should be able to handle complex arithmetic expressions that include brackets" do
-      interpreter = Ked::Interpreter.new "3 plus (4 times (2 awayFrom 5))"
-      interpreter.interpret.should eq 15
+      interpreter = Ked::Interpreter.new "remember €x = 3 plus (4 times (2 awayFrom 5)) like"
+      interpreter.interpret
+      interpreter.global_scope["x"].should eq 15
     end
 
-    it "should throw an error if the expression does match either of these patterns" do
-      interpreter = Ked::Interpreter.new "7 x 3"
+    it "should throw an error if an assignment statement is empty" do
+      interpreter = Ked::Interpreter.new "remember like"
       expect_raises(Exception) do
         interpreter.interpret
       end
