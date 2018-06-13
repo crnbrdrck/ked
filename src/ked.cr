6029556a673043e20a788f215bdc0b6b8b043ece
Ciaran Broderick <broderickciaran@gmail.com>
2018-06-13 01:45:28 +0100

    Changed the src/ked file to work more like an entrypoint
    If no args are passed, a help message will be displayed.
    
    If an arg is passed, the script will attempt to open it as if it were a ked
    script, interpret the script and currently will print out the global variable
    scope the interpreter has after the script is interpreted

--- src/ked.cr
+++ src/ked.cr
@@ -2,20 +2,35 @@ require "./ked/*"
 
 # TODO: Write documentation for `Ked`
 module Ked
-  # TODO: Put your code here
+  # NO_INPUT_FILE: help message to display when no input file is specified for the interpreter
+  NO_INPUT_FILE = "\u001b[31mked\u001b[0m: The first Corkonian scripting language.
+See the language specification at https://adamlynch.com/ked/.
+
+Currently this interpreter is a WIP.
+Supported features:\u001b[36m
+  - Variable assignment
+\u001b[0m
+To see the code and maybe even contribute, visit https://github.com/crnbrdrck/ked
+
+Usage:
+  ked feen.ked - Run the script called `feen.ked` and print the global variable scope of the interpreter after interpreting the whole thing
+
+"
 end
 
-# Run the main loop
-while true
+# Entrypoint; get the file to execute from argv and attempt to run it (for now, print out global scope)
+if ARGV.size == 0
+  puts Ked::NO_INPUT_FILE
+else
+  # Get the file name from argv and run it
+  file = ARGV[0]
   begin
-    print "calc> "
-    text = gets(chomp: true)
+    text = File.read file
   rescue
-    break
+    puts "\u001b[31mFile '#{file}' could not be read\u001b[0m"
+    Process.exit 1
   end
-  if text
-    # Try and interpret the text
-    interpreter = Ked::Interpreter.new text
-    puts interpreter.interpret
-  end
+  interpreter = Ked::Interpreter.new text
+  interpreter.interpret
+  puts interpreter.global_scope
 end
