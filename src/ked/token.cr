5063df913640d4af79ec8aa830f2b91457d3d80b
Ciaran Broderick <broderickciaran@gmail.com>
2018-06-24 00:42:43 +0100

    Initial token class finished
    This token class contains all the token types to handle example1.ked

--- src/ked/token.cr
+++ src/ked/token.cr
@@ -1,6 +1,31 @@
 module Ked
-  # Might have to extend this later but we'll see
-  alias TokenType = String
+  enum TokenType
+    # Extra, special tokens
+    ILLEGAL # Unknown token
+    EOF     # End of file
+
+    # Identifiers / Literals
+    IDENT      # add, foobar, x, y, etc
+    VAR_PREFIX # € symbol denotes variables
+    NUMBER     # integers and floats are both number tokens but will use Int and Float internally (inlike JS >.>)
+
+    # Operators
+    ASSIGN # = for assignment
+    PLUS   # 'plus'
+
+    # Delimiters
+    COMMA  # ','
+    LIKE   # 'like', ked's version of ;
+    LPAREN # '('
+    RPAREN # ')'
+    LBRACE # '{'
+    RBRACE # '}'
+
+    # Keywords
+    FUNCTION # 'bai' for function definition
+    REMEMBER # 'remember' for variable assignment
+    RETURN   # 'hereYaGoBai' for returning from a function
+  end
 
   # Class for managing Tokens
   class Token
