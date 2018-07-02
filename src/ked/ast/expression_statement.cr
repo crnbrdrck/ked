b6335127f265efbe998c59d737c406841593a663
crnbrdrck <broderickciaran@gmail.com>
2018-07-02 16:53:04 +0100

    Created a Node for expression_statements

---
+++ src/ked/ast/expression_statement.cr
@@ -0,0 +1,31 @@
+require "./expression"
+require "./statement"
+
+module Ked
+  module AST
+    # Class for handling ExpressionStatement nodes.
+    #
+    # An "ExpressionStatement" is a statement that consists of a single expression and nothing else i.e. `€x plus 10 like`.
+    #
+    # This is used solely so we can add expressions that are on their own to our `statements` list of our program node.
+    class ExpressionStatement < Statement
+      # The `Ked::Token` instance used to create this node
+      @token : Token
+      # The expression contained within this statement node
+      @value : Expression
+
+      def initialize(@token : Token, @value : Expression)
+      end
+
+      # Generate the token_literal for this node by returning the literal of the token used to create it
+      def token_literal : String
+        @token.literal
+      end
+
+      # The `Ked::Token` instance used to create this node
+      getter token
+      # The expression contained within this statement node
+      getter value
+    end
+  end
+end
